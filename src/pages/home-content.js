bfd7bf8c8c7e581020f50e8d17348dd5ce03bf47
Fasih <fasihislam1999@gmail.com>
2022-07-01 11:18:09 +0400

    fixed some links

--- src/pages/home-content.js
+++ src/pages/home-content.js
@@ -1,6 +1,8 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
+import {Link} from 'react-router-dom';
+
 const Homecontent = () => {
     return (
         <div>
@@ -10,8 +12,8 @@ const Homecontent = () => {
                 <div class="col-md-10 col-lg-8 col-xl-7">
 
                     <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Donate a Car Today!</h2>
+                        <a href="">
+                            <h2 class="post-title"><Link to="/DonateToday">Donate a Car Today!</Link></h2>
 
                         </a>
                         <p class="post-meta">
@@ -27,7 +29,7 @@ const Homecontent = () => {
                     <hr class="my-4" />
 
                     <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">Your Dependable Choice</h2></a>
+                        <a href=""><h2 class="post-title"><Link to="/DonateToday">Your Dependable Choice</Link></h2></a>
                         <p class="post-meta">
                         Your automotive contribution has value in terms of money and sentiment. We promise to uphold both as part of our donor support commitment. The donation of your charity automobile will be carefully managed for you with honesty and attention to timely details so that you may give with ease to the cause that is most important to you.
                         </p>
@@ -37,7 +39,7 @@ const Homecontent = () => {
 
                     <div class="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">Donate Now and Make a Difference</h2>
+                            <h2 class="post-title"><Link to="/DonateToday">Donate Now and Make a Difference</Link></h2>
                         </a>
                         <p class="post-meta">
                         The charity that means the most to you benefits from your used automobile contributions. Through InTow, you may choose to donate your car to ANY recognised Canadian charity, and we'll see to it that they have a simple time accepting it. Since there are no costs associated with participating in our gift-receiving programme, your charity of choice may use every dollar you donate to support the important work they are doing on behalf of Canadians just like you.
@@ -45,13 +47,6 @@ const Homecontent = () => {
 
                         <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href=""><Link style={{color: "white "}} to="/DonateToday" id='page1'>Donate Now →</Link></a></div>
                     </div>
-
-                    <hr class="my-4" />
-
-
-  
-
-
                 </div>
             </div>
         </div>
