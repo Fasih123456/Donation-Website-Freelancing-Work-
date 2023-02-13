81520b10eb50c7b92bdb11646d5fa3fee25b2594
Fasih <fasihislam1999@gmail.com>
2023-02-13 13:30:32 -0400

    made some personal changes to website

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
           </div>
         </nav>
       </div>
-      <header class="masthead" style={{ backgroundImage: "url('img/shutterstock.png')" }}>
+      <header class="masthead" style={{ backgroundImage: "url('img/main-picture/1.jpg')" }}>
         <div class="container position-relative px-4 px-lg-5">
           <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
